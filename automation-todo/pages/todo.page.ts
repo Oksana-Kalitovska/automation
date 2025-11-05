a33b0ec643e6631d95b1ed94c1456e39f90846da
oksanadrm <oksanadrm@gmail.com>
2025-11-05 12:13:09 +0200

    updated

--- automation-todo/pages/todo.page.ts
+++ automation-todo/pages/todo.page.ts
@@ -1,44 +1,43 @@
-import { Page, Locator, expect } from '@playwright/test';
+import { Page, Locator, expect } from '@playwright/test'
 
-export default class TodoPage {   // default = can be imported with any name
+export default class TodoPage {
+  // default = can be imported with any name
 
-    readonly page: Page;
-    readonly input: Locator;
-    readonly toDoItems: Locator;
-    readonly filters: Locator;
-    readonly clearCompletedButton: Locator;
+  readonly page: Page
+  readonly input: Locator
+  readonly toDoItems: Locator
+  readonly filters: Locator
+  readonly clearCompletedButton: Locator
 
-    constructor(page: Page) {
-        this.page = page;
-        this.input = page.locator('.new-todo');
-        this.toDoItems = page.locator('.todo-list li');
-        this.filters = page.locator('.filters li a');
-        this.clearCompletedButton = page.locator('.destroy');
-}
+  constructor(page: Page) {
+    this.page = page
+    this.input = page.locator('.new-todo')
+    this.toDoItems = page.locator('.todo-list li')
+    this.filters = page.locator('.filters li a')
+    this.clearCompletedButton = page.locator('.destroy')
+  }
   async open() {
-    await this.page.goto('https://demo.playwright.dev/todomvc/');
+    await this.page.goto('https://demo.playwright.dev/todomvc/')
   }
 
   async addToDo(text: string) {
-    await this.input.fill(text);
-    await this.input.press('Enter');
+    await this.input.fill(text)
+    await this.input.press('Enter')
   }
 
-    async toggleToDo(index: number) {
-        await this.toDoItems.nth(index).locator('.toggle').click(); // Toggle the checkbox of the to-do item at the specified index
-    }
+  async toggleToDo(index: number) {
+    await this.toDoItems.nth(index).locator('.toggle').click() // Toggle the checkbox of the to-do item at the specified index
+  }
 
-    async deleteToDo(index: number) {
-        await this.toDoItems.nth(index).hover(); // Hover over the to-do item to reveal the delete button
-        await this.toDoItems.nth(index).locator('.destroy').click();
-    }
+  async deleteToDo(index: number) {
+    await this.toDoItems.nth(index).hover() // Hover over the to-do item to reveal the delete button
+    await this.toDoItems.nth(index).locator('.destroy').click()
+  }
 
-    async filterToDos(filter: 'All' | 'Active' | 'Completed') {
-        await this.page.getByRole('link', { name: filter }).click();
-}
-    async assertToDoCount(count: number) {
-        await expect(this.toDoItems).toHaveCount(count);
-
-}
-
+  async filterToDos(filter: 'All' | 'Active' | 'Completed') {
+    await this.page.getByRole('link', { name: filter }).click()
+  }
+  async assertToDoCount(count: number) {
+    await expect(this.toDoItems).toHaveCount(count)
+  }
 }
--- my-new-playwright-project/setup/hooks.js
+++ my-new-playwright-project/setup/hooks.js
@@ -1,34 +1,32 @@
 const require = require('playwright') // імпортує бібліотеку Playwright для використання її функціональності у файлі.
-const { Before, After, BeforeAll, AfterAll } = require('@cucumber/cucumber');  // імпортує необхідні функції з бібліотеки Cucumber для налаштування хуків.
+const { Before, After, BeforeAll, AfterAll } = require('@cucumber/cucumber') // імпортує необхідні функції з бібліотеки Cucumber для налаштування хуків.
 
 BeforeAll(async function () {
-    // Цей хук виконується один раз перед усіма сценаріями.
-    // Тут можна ініціалізувати глобальні ресурси, наприклад, запуск браузера.
-    console.log('Launch Browser');
-    global.browser = await require.chromium.launch({ headless: false }); // Запускає браузер Chromium у видимому режимі.
-});
+  // Цей хук виконується один раз перед усіма сценаріями.
+  // Тут можна ініціалізувати глобальні ресурси, наприклад, запуск браузера.
+  console.log('Launch Browser')
+  global.browser = await require.chromium.launch({ headless: false }) // Запускає браузер Chromium у видимому режимі.
+})
 
 AfterAll(async function () {
-    // Цей хук виконується один раз після всіх сценаріїв.
-    // Тут можна звільнити глобальні ресурси, наприклад, закрити браузер.
-    console.log('Close Browser');
-    await global.browser.close(); // Закриває браузер.
-}
-);
+  // Цей хук виконується один раз після всіх сценаріїв.
+  // Тут можна звільнити глобальні ресурси, наприклад, закрити браузер.
+  console.log('Close Browser')
+  await global.browser.close() // Закриває браузер.
+})
 
 Before(async function (scenario) {
-    // Цей хук виконується перед кожним сценарієм.
-    // Тут можна створити новий контекст і сторінку для кожного сценарію.
-    console.log(`Start Scenario: ${scenario.pickle.name}`);
-    global.context = await global.browser.newContext(); // Створює новий контекст браузера.
-    global.page = await global.context.newPage(); // Створює нову сторінку в цьому контексті.
-}
-);
+  // Цей хук виконується перед кожним сценарієм.
+  // Тут можна створити новий контекст і сторінку для кожного сценарію.
+  console.log(`Start Scenario: ${scenario.pickle.name}`)
+  global.context = await global.browser.newContext() // Створює новий контекст браузера.
+  global.page = await global.context.newPage() // Створює нову сторінку в цьому контексті.
+})
 
 After(async function (scenario) {
-    // Цей хук виконується після кожного сценарію.
-    // Тут можна закрити сторінку і контекст, а також обробити результати сценарію.
-    console.log(`End Scenario: ${scenario.pickle.name}`);
-    await global.page.close(); // Закриває сторінку.
-    await global.context.close(); // Закриває контекст браузера.
-});
+  // Цей хук виконується після кожного сценарію.
+  // Тут можна закрити сторінку і контекст, а також обробити результати сценарію.
+  console.log(`End Scenario: ${scenario.pickle.name}`)
+  await global.page.close() // Закриває сторінку.
+  await global.context.close() // Закриває контекст браузера.
+})
